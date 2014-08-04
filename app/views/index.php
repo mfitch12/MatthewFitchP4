6d23f48090f45a892df4fe504b950ee8a5336502
Matt Fitch <mrf088@gmail.com>
2014-08-04 18:40:39 -0400

    added landing page

--- app/views/index.php
+++ app/views/index.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Laravel PHP Framework</title>
+	<title>Matthew Fitch Project 4</title>
 
 </head>
 <body>
+
+Project 4 goes here.
 
 </body>
 </html>
