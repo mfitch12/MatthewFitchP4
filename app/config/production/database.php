7f05f3aa0b4e52aeee2c4554b6d8b514fd0714d6
Matt Fitch <mrf088@gmail.com>
2014-08-05 14:16:01 -0400

    production database

---
+++ app/config/production/database.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'database-name-goes-here',
+            'username'  => 'root',
+            'password'  => 'mysql-password-goes-here',
+        ),
+
+    ),
+
+);
