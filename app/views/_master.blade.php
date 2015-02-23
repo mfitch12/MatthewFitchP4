f00a3a5d7a2e455ff2d431a7d8253478301d04e6
Matt Fitch <mrf088@gmail.com>
2015-02-23 12:27:58 -0500

    added capstone version

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Fitch - Project 4</title>
+    <title>Fitch - Capstone 1.0</title>
 
     <link href='{{ asset('css/bootstrap.min.css') }}' rel='stylesheet'>
     <link href='{{ asset('css/style.css') }}' rel="stylesheet">
